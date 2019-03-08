ad5a2ea14b34a7333263d4c473427c389a64a007
Patrick José Pereira <patrickelectric@gmail.com>
2019-03-07 18:01:06 -0800

    Bottom detect works
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- ArduSub/surface_bottom_detector.cpp
+++ ArduSub/surface_bottom_detector.cpp
@@ -24,24 +24,45 @@ void Sub::update_surface_and_bottom_detector()
     // check that we are not moving up or down
     bool vel_stationary = velocity.z > -0.05 && velocity.z < 0.05;
 
+    static float alt_control_error = pos_control.get_alt_error();
+    static int maybe_bottom = 0;
+
+    if(pos_control.get_alt_error() < 0 && (pos_control.get_alt_error() - alt_control_error) > 0) {
+        maybe_bottom += 1;
+    } else if(pos_control.get_alt_error() < 0) {
+        alt_control_error = pos_control.get_alt_error();
+    } else {
+        maybe_bottom = 0;
+    }
+
+    gcs_send_text_fmt(MAV_SEVERITY_INFO, "> %d | %d\n", alt_control_error < 0, (pos_control.get_alt_error() - alt_control_error) > 0);
+    gcs_send_text_fmt(MAV_SEVERITY_INFO, "vs:%d|vz:%.2f|alt_e:%.2f|altce:%.2f|mb %d\n", vel_stationary, velocity.z, pos_control.get_alt_error(), alt_control_error, maybe_bottom);
+    // TODO: REmove
+    current_depth = barometer.get_altitude(); // cm
+    gcs_send_text_fmt(MAV_SEVERITY_INFO, "alt_set: %.3f \t alt: %.3f\n", pos_control.get_alt_target(), inertial_nav.get_altitude());
+    gcs_send_text_fmt(MAV_SEVERITY_INFO, "cd: %.3f\n", current_depth);
+
+    //printf("%d | %f \t %f \t %f\n", vel_stationary, velocity.z, pos_control.get_alt_error(), pos_control.get_alt_target());
+
+    const float surface_depth = g.surface_depth.get()/100.0f;
     if (ap.depth_sensor_present && sensor_health.depth) { // we can use the external pressure sensor for a very accurate and current measure of our z axis position
         current_depth = barometer.get_altitude(); // cm
 
 
         if (ap.at_surface) {
-            set_surfaced(current_depth > g.surface_depth/100.0 - 0.05); // add a 5cm buffer so it doesn't trigger too often
+            set_surfaced(current_depth > surface_depth - 0.05); // add a 5cm buffer so it doesn't trigger too often
         } else {
-            set_surfaced(current_depth > g.surface_depth/100.0); // If we are above surface depth, we are surfaced
+            set_surfaced(current_depth > surface_depth); // If we are above surface depth, we are surfaced
+        }
+        // Vehicle can't be in surface and bottom at the same time
+        if(ap.at_surface) {
+            return;
         }
 
 
-        if (motors.limit.throttle_lower && vel_stationary) {
+        if ((motors.limit.throttle_lower && vel_stationary) || maybe_bottom > 2) {
             // bottom criteria met - increment the counter and check if we've triggered
-            if (bottom_detector_count < ((float)BOTTOM_DETECTOR_TRIGGER_SEC)*MAIN_LOOP_RATE) {
-                bottom_detector_count++;
-            } else {
-                set_bottomed(true);
-            }
+            set_bottomed(true);
 
         } else {
             set_bottomed(false);
@@ -52,19 +73,11 @@ void Sub::update_surface_and_bottom_detector()
         if (motors.limit.throttle_upper) {
 
             // surface criteria met, increment counter and see if we've triggered
-            if (surface_detector_count < ((float)SURFACE_DETECTOR_TRIGGER_SEC)*MAIN_LOOP_RATE) {
-                surface_detector_count++;
-            } else {
-                set_surfaced(true);
-            }
+            set_surfaced(true);
 
         } else if (motors.limit.throttle_lower) {
             // bottom criteria met, increment counter and see if we've triggered
-            if (bottom_detector_count < ((float)BOTTOM_DETECTOR_TRIGGER_SEC)*MAIN_LOOP_RATE) {
-                bottom_detector_count++;
-            } else {
-                set_bottomed(true);
-            }
+            set_bottomed(true);
 
         } else { // we're not at the limits of throttle, so reset both detectors
             set_surfaced(false);
