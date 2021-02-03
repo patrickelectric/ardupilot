6a3c656ef44e6b6621181080cf7dd9087efbc5ad
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-03 11:28:31 -0300

    AP_KDECAN: Remove unnecessary return
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- libraries/AP_KDECAN/AP_KDECAN.cpp
+++ libraries/AP_KDECAN/AP_KDECAN.cpp
@@ -173,8 +173,6 @@ void AP_KDECAN::init(uint8_t driver_index, bool enable_filters)
     _initialized = true;
 
     debug_can(AP_CANManager::LOG_DEBUG, "init done");
-
-    return;
 }
 
 void AP_KDECAN::loop()
