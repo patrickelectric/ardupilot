5f6e941bcc4e07b79950101a35361b9441c56c46
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-03 09:43:34 -0300

    AP_LandingGear: Remove else after return
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- libraries/AP_LandingGear/AP_LandingGear.cpp
+++ libraries/AP_LandingGear/AP_LandingGear.cpp
@@ -163,9 +163,8 @@ bool AP_LandingGear::deployed()
 {
     if (_pin_deployed == -1) {
         return _deployed;
-    } else {
-        return hal.gpio->read(_pin_deployed) == _pin_deployed_polarity ? true : false;
-    }
+    }         return hal.gpio->read(_pin_deployed) == _pin_deployed_polarity ? true : false;
+   
 }
 
 AP_LandingGear::LG_WOW_State AP_LandingGear::get_wow_state()
